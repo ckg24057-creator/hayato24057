527b8a79bd0b0fc1d365ac530e1c9235f199072b
アガタ　ハヤト <ckg24057@st.ckg.ac.jp>
2026-03-09 11:33:45 +0900

    作業０:　環境構築

--- app/main.py
+++ app/main.py
@@ -2,10 +2,8 @@ from fastapi import FastAPI, Request
 from fastapi.templating import Jinja2Templates
 
 app = FastAPI()
-# templatesフォルダを指定
 templates = Jinja2Templates(directory="app/templates")
 
 @app.get("/")
 def read_root(request: Request):
-    # index.htmlにリクエスト情報を渡して表示
     return templates.TemplateResponse("index.html", {"request": request})
